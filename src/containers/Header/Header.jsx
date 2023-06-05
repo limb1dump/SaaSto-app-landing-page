e960dd10a4718df400e7a0eb7b6d04d0eef07b0c
limb1dump <skywalkervaider@gmail.com>
2023-06-05 10:31:25 +0300

    Responsive nav and header

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -8,37 +8,23 @@ import { images } from '../../constants';
 const Header = () => {
     return (
         <div className='header'>
-            <div className="header__shapes">
-                <div className="header__sinus">
-                    <img src={images.sinus} alt="shape" />
-                </div>
-                <div className="header__curves">
-                    <img src={images.curves} alt="lines" />
-                </div>
-                <div className="header__sharp-sinus">
-                    <img src={images.sharpSinus} alt="shape" />
-                </div>
-                <div className="header__thick">
-                    <img src={images.thick} alt="shape" />
-                </div>
-                <div className="header__dimond">
-                    <img src={images.diamond} alt="shape" />
-                </div>
-                <div className="header__arrow-loop">
-                    <img src={images.arrowLoop} alt="shape" />
-                </div>
-            </div>
             <div className='container header__container '>
                 <div className="header__body">
                     <h1 className="header__title">
                         A collaborative Time Tracking that you Need
                     </h1>
-                    <p className="header_text">
-                        An enim nullam tempor sapien gravida donec enim ipsum porta justo congue magna at pretium purus pretium ligula
+                    {/* title_mobile needed because title text differs a bit in mobile version */}
+                    <h1 className="header__title_mobile">
+                        A Collaborative  Time Tracking That You Need
+                    </h1>
+                    <p className="header__text">
+                        An enim nullam tempor sapien gravida donec enim ipsum porta justo &nbsp;congue magna at pretium purus pretium ligula
                     </p>
-                    <Button btnHeader='btn-header'>
-                        Start 14 Days Trial
-                    </Button>
+                    <div className='btn-wrapper'>
+                        <Button btnHeader='btn-header'>
+                            Start 14 Days Trial
+                        </Button>
+                    </div>
                 </div>
             </div>
         </div>
